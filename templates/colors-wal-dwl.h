056ebce697b5fe47f09f5afa0d8a5a8ef5bd35cf
A S H <shadowash8@protonmail.com>
2026-03-08 07:13:19 +0530

    style: fix dwl colors formatting

--- templates/colors-wal-dwl.h
+++ templates/colors-wal-dwl.h
@@ -1,10 +1,10 @@
 /* Taken from https://github.com/djpohly/dwl/issues/466 */
-#define COLOR(hex)    {{ ((hex >> 24) & 0xFF) / 255.0f, \
+#define COLOR(hex)    { ((hex >> 24) & 0xFF) / 255.0f, \
                         ((hex >> 16) & 0xFF) / 255.0f, \
                         ((hex >> 8) & 0xFF) / 255.0f, \
-                        (hex & 0xFF) / 255.0f }}
+                        (hex & 0xFF) / 255.0f }
 
-static const float rootcolor[]             = COLOR(0x{color0.strip}ff);
-static const float bordercolor[]           = COLOR(0x{color8.strip}ff);
-static const float focuscolor[]            = COLOR(0x{color2.strip}ff);
-static const float urgentcolor[]           = COLOR(0x{color1.strip}ff);
+static const float rootcolor[4]             = COLOR(0x{color0.strip}ff);
+static const float bordercolor[4]           = COLOR(0x{color8.strip}ff);
+static const float focuscolor[4]            = COLOR(0x{color2.strip}ff);
+static const float urgentcolor[4]           = COLOR(0x{color1.strip}ff);
